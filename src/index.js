f69a0cd5765ed5f03cb931b7535e473524cb71c3
Pherpher089 <Christutor089@gmail.com>
2020-06-23 18:15:58 -0700

    Added new env vars for db

--- src/index.js
+++ src/index.js
@@ -74,6 +74,8 @@ const startServer = async () => {
 
   // Connect to mongoose using DATABASE_URL and await the promise to resolve
   await mongoose.connect(DATABASE_URL, {
+    username: process.env.USE,
+    password: process.env.PASS,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
